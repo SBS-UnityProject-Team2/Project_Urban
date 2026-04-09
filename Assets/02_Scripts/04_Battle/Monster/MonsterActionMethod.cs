46c321ed04e5f57f1a1509f851efb1ebbab2d722
JunPyoSeo <cooljp894@gmail.com>
2026-04-09 21:46:04 +0900

    [FEAT] 플레이어 데미지 이펙트 추가

--- Assets/02_Scripts/04_Battle/Monster/MonsterActionMethod.cs
+++ Assets/02_Scripts/04_Battle/Monster/MonsterActionMethod.cs
@@ -51,12 +51,16 @@ public static class MonsterActionMethod
     public static async UniTask Charge(Actor source) // 8000
     {
         Attack(source, ElementType.None, 6);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask Bite(Actor source) // 8001
     {
         Attack(source, ElementType.None, 3);
         GiveEffect(source, Battle.Instance.Player, StatusEffectName.Bleed, 1, 3);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask Screw(Actor source) // 8002
@@ -64,13 +68,16 @@ public static class MonsterActionMethod
         for (int i = 0; i < 2; i++)
         {
             Attack(source, ElementType.None, 5);
-            await UniTask.WaitForSeconds(0.25f);
+
+            await EffectManager.Instance.PlayerHitEffect();
         }
     }
 
     public static async UniTask ScarpGun(Actor source) // 8003
     {
         Attack(source, ElementType.None, 12);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask CrudeMolotv(Actor source) // 8004
@@ -81,6 +88,8 @@ public static class MonsterActionMethod
     {
         Attack(source, ElementType.Ruin, 10);
         GiveEffect(source, source, StatusEffectName.Reinforce, -2, 3);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask PsychicBeat(Actor source) // 8006
@@ -93,13 +102,16 @@ public static class MonsterActionMethod
     {
         Attack(source, ElementType.Psychic, 9);
         AddBlock(source, 9);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask Bash(Actor source) // 8008
     {
         Attack(source, ElementType.Bio, 9);
-
         GiveEffect(source, Battle.Instance.Player, StatusEffectName.Weaken, 2, 3);
+
+        await EffectManager.Instance.PlayerHitEffect();
     }
 
     public static async UniTask CorrosiveShock(Actor source) // 8009
@@ -107,7 +119,7 @@ public static class MonsterActionMethod
         for (int i = 0; i < 2; i++)
         {
             Attack(source, ElementType.Bio, 7);
-            await UniTask.WaitForSeconds(0.25f);
+            await EffectManager.Instance.PlayerHitEffect();
         }
         
         GiveEffect(source, Battle.Instance.Player, StatusEffectName.Broken, 2, 3);
@@ -118,7 +130,7 @@ public static class MonsterActionMethod
         for (int i = 0; i < 5; i++)
         {
             Attack(source, ElementType.None, 3);
-            await UniTask.WaitForSeconds(0.1f);
+            await EffectManager.Instance.PlayerHitEffect();
         }
     }
     #endregion
@@ -127,34 +139,46 @@ public static class MonsterActionMethod
     public static async UniTask Curl(Actor source) // 8200
     {
         AddBlock(source, 7);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask Smoke(Actor source) // 8201
     {
         AddBlock(source, 10);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask ScrapShield(Actor source) // 8202
     {
         AddBlock(source, 15);
         GiveEffect(source, source, StatusEffectName.Broken, 2, 3);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask RegenCell(Actor source) // 8204
     {
         AddBlock(source , 7);
         GiveEffect(source, source, StatusEffectName.Regeneration, -3, 2);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask Barrier(Actor source) // 8205
     {
         AddBlock(source, 12);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask Rush(Actor source) // 8206
     {
         AddBlock(source, 8);
         GiveEffect(source, source, StatusEffectName.Reinforce, -2, 2);
+
+        await UniTask.CompletedTask;
     }
     #endregion
 
@@ -162,31 +186,41 @@ public static class MonsterActionMethod
     public static async UniTask Sharpening(Actor source) // 8400
     {
         GiveEffect(source, source, StatusEffectName.Burst, -2, 9);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask EnhanceBody(Actor source) // 8401
     {
         GiveEffect(source, source, StatusEffectName.Armor, -2, 2);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask GhostPill(Actor source) // 8402
     {
         GiveEffect(source, source, StatusEffectName.Blur, -2, 1);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask ToxicSpore(Actor source) // 8403
     {
-        
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask Enrage(Actor source) // 8404
     {
         GiveEffect(source, source, StatusEffectName.Reinforce, -2, 3);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask Reload(Actor source) // 8405
     {
         GiveEffect(source, source, StatusEffectName.LoadedIncendiary, 1, 4);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask DefiledInjection(Actor source) // 8406
@@ -202,6 +236,8 @@ public static class MonsterActionMethod
         payload.Write(ElementType.Bio);
 
         ActionBus.Dispatch(payload);
+
+        await UniTask.CompletedTask;
     }
     #endregion
 
@@ -209,11 +245,15 @@ public static class MonsterActionMethod
     public static async UniTask Roar(Actor source) // 8600
     {
         GiveEffect(source, Battle.Instance.Player, StatusEffectName.Weaken, 2, 3);
+
+        await UniTask.CompletedTask;
     }
 
     public static async UniTask ArmorBreak(Actor source) // 8601
     {
         // GiveEffect(source, Battle.Instance.Player, StatusEffectName.Armor)
+
+        await UniTask.CompletedTask;
     }
     #endregion
 
