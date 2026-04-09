670950f73ffa140ee556951c7d3c5187cc96c67a
An-Shine <akh1012@naver.com>
2026-04-10 03:13:04 +0900

    Update Battle.cs

--- Assets/02_Scripts/04_Battle/Battle.cs
+++ Assets/02_Scripts/04_Battle/Battle.cs
@@ -82,7 +82,7 @@ public class Battle : SceneSingleton<Battle>
         }
 
         IsPause = true;
-        PlayerManager.Instance.Health.SetHp(Player.Status.Health.CurHp);
+        PlayerManager.Instance.Health.SetCurrentHealth(Player.Status.Health.CurHp);
     }
 
     private void ResultHPCoin(bool isPlayerWin)
